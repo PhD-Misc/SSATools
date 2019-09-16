910f9b9019bba81d734a48d126a4f4ccdd6df111
steveprabu <steverajprabu@gmail.com>
2019-09-16 15:04:30 +0800

    added setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+        name="SSATools",
+        version='0.1',        
+        scripts=['parallax'],
+        author="Steve Prabu",
+        description="custom tools",
+        packages=setuptools.find_packages(),
+        author_email="",url="https://github.com/StevePrabu/SSATools.git",
+)
+
